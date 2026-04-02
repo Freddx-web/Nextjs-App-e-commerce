2de8927f608137cbba842cd54f1229cd6f40481c
Freddx-web <fredanger299@gmail.com>
2026-04-02 12:56:52 -0400

    Version 1.7.0 Fix

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ["next/core-web-vitals"],
+  plugins: ["@next/next"],
+  rules: {
+    // Add any custom rules here
+  },
+  settings: {
+    next: {
+      rootDir: "./",
+    },
+  },
+};
